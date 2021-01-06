f67dce1fd199b8c7339fa310be26c73536419252
Samuel Schnurr <samuelschnurr@live.de>
2021-01-06 15:49:21 +0100

    Portfolio details component is created

---
+++ web/StockBuddyApp/src/app/portfolio/portfolio-details/portfolio-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PortfolioDetailsComponent } from './portfolio-details.component';
+
+describe('PortfolioDetailsComponent', () => {
+  let component: PortfolioDetailsComponent;
+  let fixture: ComponentFixture<PortfolioDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PortfolioDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PortfolioDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

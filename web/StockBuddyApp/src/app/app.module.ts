dc5fce2767e87815dd8e9543bc9bda740c027285
Samuel Schnurr <samuelschnurr@live.de>
2021-01-10 17:14:15 +0100

    Embedded navbar in app component

--- web/StockBuddyApp/src/app/app.module.ts
+++ web/StockBuddyApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StockModule } from './stock/stock.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { StockModule } from './stock/stock.module';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     StockModule
   ],
   providers: [],

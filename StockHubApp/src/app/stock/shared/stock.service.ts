009f41ea3e2441169b1079a1050952699d319cdf
Samuel Schnurr <samuelschnurr@live.de>
2021-01-22 21:55:09 +0100

    Added typedef for postStock result

--- StockHubApp/src/app/stock/shared/stock.service.ts
+++ StockHubApp/src/app/stock/shared/stock.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Stock } from './stock.model';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 /**
  * Class Definition of Stock.
@@ -12,16 +13,17 @@ export class StockService {
   public formData: Stock = new Stock();
   private readonly baseUrl = '...';
 
- /**
-  * Initializes a new instance of the StockService class.
-  * @param httpClient The via dependency injection loaded HttpClient.
-  */
+  /**
+   * Initializes a new instance of the StockService class.
+   * @param httpClient The via dependency injection loaded HttpClient.
+   */
   public constructor(private httpClient: HttpClient) { }
 
   /**
    * Posts the formData to the backend.
+   * @returns A observable object of the post response.
    */
-  public postStock() {
+  public postStock(): Observable<object> {
     return this.httpClient.post(this.baseUrl, this.formData);
   }
 }

63e65a046fd90cab40fa06da39a3b4bfe479832a
Samuel Schnurr <samuelschnurr@live.de>
2021-01-22 21:52:49 +0100

    Added comments

--- StockHubApp/src/app/stock/shared/stock.service.ts
+++ StockHubApp/src/app/stock/shared/stock.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Stock } from './stock.model';
 import { HttpClient } from '@angular/common/http';
 
+/**
+ * Class Definition of Stock.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +12,15 @@ export class StockService {
   public formData: Stock = new Stock();
   private readonly baseUrl = '...';
 
+ /**
+  * Initializes a new instance of the StockService class.
+  * @param httpClient The via dependency injection loaded HttpClient.
+  */
   public constructor(private httpClient: HttpClient) { }
 
+  /**
+   * Posts the formData to the backend.
+   */
   public postStock() {
     return this.httpClient.post(this.baseUrl, this.formData);
   }

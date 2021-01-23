1e92b9d34197486ec6ff3c98c7b6d6f8df6a04e7
Samuel Schnurr <samuelschnurr@live.de>
2021-01-23 18:04:14 +0100

    Renamed project properties to StockHub

--- StockHubApi/StockHubApi/Startup.cs
+++ StockHubApi/StockHubApi/Startup.cs
@@ -18,7 +18,6 @@ namespace StockHubApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
         }
 

0c112a00653b74a8683f3094252a7d3068f25be7
Samuel Schnurr <samuelschnurr@live.de>
2021-01-23 18:53:43 +0100

    Modified comments for stock Model

--- StockHubApp/src/app/stock/shared/stock.model.ts
+++ StockHubApp/src/app/stock/shared/stock.model.ts
@@ -1,8 +1,8 @@
 export class Stock {
     public id = 0;
     public name = '';
-    /** The amount of the stock units in the format 0.000 */
+    /** The amount of the stock units in the format 0.0000 */
     public amount = 0; //
-    /** The acquisition Price per Unit in the currency format 0.00 */
+    /** The acquisition Price per Unit in the currency format 0.0000 */
     public acquisitionPricePerUnit = 0;
 }

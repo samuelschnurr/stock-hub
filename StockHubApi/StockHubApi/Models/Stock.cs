2e30b327a103c31d155e12ceba31c2af40575bfa
Samuel Schnurr <samuelschnurr@live.de>
2021-01-24 12:20:32 +0100

    Added stock model validation attributes

--- StockHubApi/StockHubApi/Models/Stock.cs
+++ StockHubApi/StockHubApi/Models/Stock.cs
@@ -1,15 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockHubApi.Models
 {
     public class Stock : Base
     {
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]
         [Column(TypeName="nvarchar(50)")]
         public string Name { get; set; }
 
+        [Range(0.0001, 9999999999999.9999)]
+        [Required]
         [Column(TypeName = "decimal(17,4)")]
         public double Amount { get; set; }
 
+        [Range(0.0001, 99999999.9999)]
+        [Required]
         [Column(TypeName = "decimal(12,4)")]
         public double AcquisitionPricePerUnit { get; set; }
     }

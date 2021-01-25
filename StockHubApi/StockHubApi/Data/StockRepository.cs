3d615ea8e2193a1e4fbb46a2dd95586930101d14
Samuel Schnurr <samuelschnurr@live.de>
2021-01-25 23:01:14 +0100

    Created StockRepository

---
+++ StockHubApi/StockHubApi/Data/StockRepository.cs
@@ -0,0 +1,20 @@
+﻿using StockHubApi.Models;
+
+namespace StockHubApi.Data
+{
+    public class StockRepository
+    {
+        private StockHubDbContext dbContext;
+
+        public StockRepository(StockHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Stock stock)
+        {
+            this.dbContext.Stocks.Add(stock);
+            this.dbContext.SaveChanges();
+        }
+    }
+}

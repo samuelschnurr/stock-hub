3e60f09cbf8443bc6b760e36d0b788abb5ede16e
Samuel Schnurr <samuelschnurr@live.de>
2021-01-26 23:33:19 +0100

    Added portfolio dashboard to routing

--- StockHubApp/src/app/core/app-routing/app-routing.module.ts
+++ StockHubApp/src/app/core/app-routing/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioDashboardComponent } from 'src/app/portfolio/portfolio-dashboard/portfolio-dashboard.component';
 import { StockDetailsFormComponent } from '../../stock/stock-details/stock-details-form/stock-details-form.component';
 import { StockDetailsComponent } from '../../stock/stock-details/stock-details.component';
 
 const routes: Routes = [
-  { path: 'first', component: StockDetailsComponent },
-  { path: 'second', component: StockDetailsFormComponent },
+  { path: '', component: PortfolioDashboardComponent },
+  { path: 'portfolio', component: PortfolioDashboardComponent },
+  { path: 'stocks', component: StockDetailsComponent },
+  { path: 'stocks/new', component: StockDetailsFormComponent },
 ];
 
 @NgModule({
--- StockHubApp/src/app/portfolio/portfolio-dashboard/portfolio-dashboard.component.ts
+++ StockHubApp/src/app/portfolio/portfolio-dashboard/portfolio-dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-dashboard',
-  templateUrl: './portfolio-dashboard.component.html',
-  styleUrls: ['./portfolio-dashboard.component.css']
+  templateUrl: './portfolio-dashboard.component.html'
 })
 export class PortfolioDashboardComponent { }

54b6378db4d13ca51b379516eb74b05e93a9a690
Samuel Schnurr <samuelschnurr@live.de>
2021-01-28 00:36:35 +0100

    Added form reset after create and update

--- StockHubApp/src/app/stock/stock-details/stock-details.component.ts
+++ StockHubApp/src/app/stock/stock-details/stock-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { StockService } from '../shared/stock.service';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Stock } from '../shared/stock.model';
 
 @Component({
   selector: 'app-stock-details',
@@ -18,10 +19,16 @@ export class StockDetailsComponent {
     }
   }
 
+  private resetForm(form: NgForm): void {
+    form.form.reset();
+    this.stockService.formData = new Stock();
+  }
+
   private insertRecord(form: NgForm): void {
     this.stockService.postStock().subscribe(
       result => {
-        console.log("success");
+        console.log('success');
+        this.resetForm(form);
         this.router.navigate(['stocks']);
       },
       error => {
@@ -33,7 +40,8 @@ export class StockDetailsComponent {
   private updateRecord(form: NgForm): void {
     this.stockService.updateStock().subscribe(
       result => {
-        console.log("success");
+        console.log('success');
+        this.resetForm(form);
         this.router.navigate(['stocks']);
       },
       error => {

10d2f742d31d3cf57fb3dededc1272309ffbb30c
Samuel Schnurr <samuelschnurr@live.de>
2021-01-27 23:09:22 +0100

    After stock post redirect to list

--- StockHubApp/src/app/stock/stock-details/stock-details.component.ts
+++ StockHubApp/src/app/stock/stock-details/stock-details.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { StockService } from '../shared/stock.service';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stock-details',
   templateUrl: './stock-details.component.html'
 })
 export class StockDetailsComponent {
-  public constructor(public stockService: StockService) { }
+  public constructor(private router: Router, public stockService: StockService) { }
 
   public onSubmit(form: NgForm): void {
     this.stockService.postStock().subscribe(
       result => {
         console.log("success");
+        this.router.navigate(['stocks']);
       },
       error => {
         console.error(error);

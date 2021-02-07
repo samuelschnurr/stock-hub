99e4e638cbf91ccc9304950251f7218c70276340
ssn <ssn@OKC25>
2021-02-07 18:58:11 +0100

    Added form validation for numer fields

--- StockHubApp/src/app/stock/shared/stock.model.ts
+++ StockHubApp/src/app/stock/shared/stock.model.ts
@@ -4,7 +4,7 @@ export class Stock {
     public id = 0;
     public name = ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]];
     /** The amount of the stock units in the format 0.0000 */
-    public amount = ['', [Validators.required]];
+    public amount = ['', [Validators.required, Validators.min(0.0001), Validators.max(9999999999999.9999)]];
     /** The acquisition Price per Unit in the currency format 0.0000 */
-    public acquisitionPricePerUnit = ['', [Validators.required]];
+    public acquisitionPricePerUnit = ['', [Validators.required, Validators.min(0.0001), Validators.max(99999999.9999)]];
 }

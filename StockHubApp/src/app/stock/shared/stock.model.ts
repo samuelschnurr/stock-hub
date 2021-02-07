ccc8579bb9f152af2b1cb9a17260f0d7305529d8
ssn <ssn@OKC25>
2021-02-07 20:14:37 +0100

    Corrected acquisition price per unit labels

--- StockHubApp/src/app/stock/shared/stock.model.ts
+++ StockHubApp/src/app/stock/shared/stock.model.ts
@@ -5,6 +5,6 @@ export class Stock {
     public name = ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]];
     /** The amount of the stock units in the format 0.0000 */
     public amount = ['', [Validators.required, Validators.min(0.0001), Validators.max(9999999999999.9999)]];
-    /** The acquisition Price per Unit in the currency format 0.0000 */
+    /** The acquisition price per unit in the currency format 0.0000 */
     public acquisitionPricePerUnit = ['', [Validators.required, Validators.min(0.0001), Validators.max(99999999.9999)]];
 }

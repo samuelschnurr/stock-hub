d3d65d2709f8475be548a0a0bbb9482e3e20b3d6
samuelschnurr <samuelschnurr@live.de>
2021-02-16 15:23:46 +0100

    Installed and registered ngx-toastr

--- StockHubApp/src/app/shared/shared.module.ts
+++ StockHubApp/src/app/shared/shared.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
   exports: [
     CommonModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule
   ]
 })
 export class SharedModule { }

91e8a51b4295aa21b8139a831c8977e831d7a854
samuelschnurr <samuelschnurr@live.de>
2021-02-16 21:17:53 +0100

    Made modal dialog component configurable

--- StockHubApp/src/app/shared/modal-dialog/modal-dialog.component.ts
+++ StockHubApp/src/app/shared/modal-dialog/modal-dialog.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal-dialog',
   templateUrl: './modal-dialog.component.html'
 })
 export class ModalDialogComponent {
+  /**
+   * The EventEmitter for the id which has been submitted by the modal dialog.
+   * Is equals to the id property if the the modal dialog was submitted, but not canceled.
+   * Is required to pass id of a record to the modal dialog and pass it to the submit function.
+   */
+  @Output() public idSubmitEvent = new EventEmitter<number>();
+  /**
+   * The id of the clicked record which opened the modal dialog.
+   * Is required to pass id of a record to the modal dialog and pass it to the submit function.
+   */
+  @Input() public id = 0;
   /**
    * The title which will be shown at the top of the modal dialog.
    * Can be set from outside, otherwise it shows a default value.
@@ -27,8 +38,9 @@ export class ModalDialogComponent {
   @Input() public submitLabel = 'Submit';
 
   /**
-   * The function which will be triggered if the the modal dialog is submitted.
-   * Can be set from outside, otherwise it does no action.
+   * Emits the idSubmitEvent with the value of the submitted id.
    */
-  @Input() public submitModal(): void {}
+  public submitModal(): void {
+    this.idSubmitEvent.emit(this.id);
+  }
 }

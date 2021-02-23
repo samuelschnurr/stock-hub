83ddb17316526d81ededff3b8c177798a2238e5d
samuelschnurr <samuelschnurr@live.de>
2021-02-23 20:37:46 +0100

    Extracted allowed cors hosts to appsettings file

--- StockHubApi/StockHubApi/Startup.cs
+++ StockHubApi/StockHubApi/Startup.cs
@@ -9,25 +9,39 @@ using StockHubApi.Utils;
 
 namespace StockHubApi
 {
+    /// <summary>
+    /// Configures services for the application at startup and handles the request pipeline.
+    /// </summary>
     public class Startup
     {
-        private const string AllowSpecificOrigins = "allowSpecificOrigins";
+        private const string AllowSpecificOrigins = "AllowSpecificOrigins";
 
+        /// <summary>
+        /// The default constructor to initialize a new instance of <see cref="Startup"/>.
+        /// </summary>
+        /// <param name="configuration">A via dependency injection loaded instance of <see cref="IConfiguration"/>.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Provides access to global configuration files like appsettings.json to configure the application.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime.
+        /// Can be used add services to the container.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> in which services will be registered.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
             {
-                options.AddPolicy(name: AllowSpecificOrigins, builder =>
+                options.AddPolicy(AllowSpecificOrigins, builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200");
+                    builder.WithOrigins(Configuration[AllowSpecificOrigins]);
                     builder.AllowAnyMethod();
                     builder.AllowAnyHeader();
                 });
@@ -35,12 +49,17 @@ namespace StockHubApi
 
             services.AddControllers();
             services.AddCustomServices();
-            services.AddCustomRepositories();        
+            services.AddCustomRepositories();
             services.AddDbContext<StockHubDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("StockHubDb")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime.
+        /// Can be used to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The via dependency injection loaded instance of <see cref="IApplicationBuilder"/>.</param>
+        /// <param name="env">The via dependency injection loaded instance of <see cref="IWebHostEnvironment"/>.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -49,17 +68,11 @@ namespace StockHubApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors(AllowSpecificOrigins);
 
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }

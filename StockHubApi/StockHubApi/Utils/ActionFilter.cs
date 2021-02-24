0561e1bbdf1f4552d235fbc924c76ec238d97ecd
samuelschnurr <samuelschnurr@live.de>
2021-02-24 19:18:35 +0100

    Added ActionFilter for logging controller entrance

---
+++ StockHubApi/StockHubApi/Utils/ActionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+/// <inheritdoc />
+public class ActionFilter : IActionFilter
+{
+    private readonly ILogger<ActionFilter> logger;
+
+    /// <summary>
+    /// The default constructor for creating a new instance of <see cref="ActionFilter" />.
+    /// </summary>
+    /// <param name="logger">The via dependency injection loaded <see cref="ILogger" />.</param>
+    public ActionFilter(ILogger<ActionFilter> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <inheritdoc />
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        logger.LogInformation($"{nameof(OnActionExecuting)}: {context?.ActionDescriptor?.DisplayName}");
+    }
+
+    /// <inheritdoc />
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        logger.LogInformation($"{nameof(OnActionExecuted)}: {context?.ActionDescriptor?.DisplayName}");
+    }
+}

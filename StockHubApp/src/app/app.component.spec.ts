a4f387d701fd2816b8aa4913fc469ca61ec17b45
samuelschnurr <samuelschnurr@live.de>
2021-03-02 00:11:39 +0100

    Removed old invalid spec.ts methods

--- StockHubApp/src/app/app.component.spec.ts
+++ StockHubApp/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it('should have as title \'StockHub\'', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('StockHub');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('StockHub app is running!');
-    });
 });
